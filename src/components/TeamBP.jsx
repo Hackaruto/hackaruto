925991aa72abf456a385ff257af4263864f69b75
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-02-04 23:59:32 +0530

    updated team section

--- src/components/TeamBP.jsx
+++ src/components/TeamBP.jsx
@@ -1,20 +1,19 @@
 import './Main.css'
 import React from 'react'
-import team from './../../resources/team.png'
 import { AiOutlineGithub } from 'react-icons/ai';
 import { TiSocialTwitter } from 'react-icons/ti';
 import { TiSocialLinkedin } from 'react-icons/ti';
 
-const TeamBP = () => {
+const TeamBP = ({imgSrc,name,designation,github,twitter,linkedin}) => {
     return (
         <div className='m-8'>
-            <img src={team} alt=""  className='rounded-3xl h-[20rem]'/>
-            <h1 className='font-bold text-lg mt-4 text-center'>Harsh</h1>
-            <h3 className='text-md mt-1 text-sky-500 text-center'>Founder & Web Lead</h3>
+            <img src={imgSrc} alt=""  className='rounded-3xl h-[20rem] w-[16rem]'/>
+            <h1 className='font-bold text-lg mt-4 text-center'>{name}</h1>
+            <h3 className='text-md mt-1 text-sky-500 text-center'>{designation}</h3>
             <div className='flex justify-center'>
-                <AiOutlineGithub className='text-4xl m-3 ml-0 bg-gray-200 rounded-[50%] p-1 text-gray-600 cursor-pointer'/>
-                <TiSocialTwitter className='text-4xl m-3 ml-0 bg-gray-200 rounded-[50%] p-1 text-gray-600 cursor-pointer'/>
-                <TiSocialLinkedin className='text-4xl m-3 ml-0 bg-gray-200 rounded-[50%] p-1 text-gray-600 cursor-pointer'/>
+                <a href={github}><AiOutlineGithub className='text-4xl m-3 ml-0 bg-gray-200 rounded-[50%] p-1 text-gray-600 cursor-pointer'/></a>
+                <a href={twitter}><TiSocialTwitter className='text-4xl m-3 ml-0 bg-gray-200 rounded-[50%] p-1 text-gray-600 cursor-pointer'/></a>
+                <a href={linkedin}><TiSocialLinkedin className='text-4xl m-3 ml-0 bg-gray-200 rounded-[50%] p-1 text-gray-600 cursor-pointer'/></a>
             </div>
         </div>
     )
