213b3bb47d50658aa5b8e0659b92a1a983cc0c5f
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-03-30 16:39:26 +0530

    added new events and projects

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,18 +1,49 @@
-import './Main.css'
-import session3 from './../../resources/session3.jpg'
-import { AiOutlineGithub } from 'react-icons/ai';
-import {BiLink} from 'react-icons/bi';
+import "./Main.css";
+import session3 from "./../../resources/session3.jpg";
+import session2 from "./../../resources/introweb3.png";
+import session1 from "./../../resources/memophilia.jpg";
+import { AiOutlineGithub } from "react-icons/ai";
+import { BiLink } from "react-icons/bi";
 
 const Events = () => {
-    return (
-        <div className='mt-10 flex flex-col'>
-            <h3 className="text-center text-[#0094FF] pt-10">Events</h3>
-        <h1 className="text-center text-[#000] font-extrabold text-4xl">
-            Upcoming Events
-        </h1>
+  const events = [
+    {
+      img: session1,
+      title: "Meme-Jutsu",
+      description: "A 10 day meme competition on instagram.",
+      platform: "Zoom",
+      date: "2-9 Apr 2021",
+      time: "",
+      eventLink: "",
+    },
+    {
+      img: session2,
+      title: "Intro to Web3",
+      description: "Introduction to Web 3.0 & Blockchain.",
+      platform: "Zoom",
+      date: "12.02.2022",
+      time: "08:00 p.m",
+      eventLink: "https://ahack.link/webinar-intro-to-web3",
+    },
+    {
+      img: session3,
+      title: "Intro to Web3",
+      description: "Introduction to Web 3.0 & Blockchain.",
+      platform: "Zoom",
+      date: "12.02.2022",
+      time: "08:00 p.m",
+      eventLink: "https://ahack.link/webinar-intro-to-web3",
+    },
+  ];
+  return (
+    <div className="mt-10 flex flex-col">
+      <h3 className="text-center text-[#0094FF] pt-10">Events</h3>
+      <h1 className="text-center text-[#000] font-extrabold text-4xl">
+        Upcoming Events
+      </h1>
 
-        <div className='grid lg:grid-cols-1 m-[auto]'>
-                <div className='max-w-xs m-12'>
+      <div className="grid md:grid-cols-1 lg:grid-cols-3 m-[auto]">
+        {/* <div className='max-w-xs m-12'>
                    <img src={session3} alt="" className='rounded-3xl' />
                    <h2 className='pt-8 pb-5 font-bold text-xl pl-0'>
                       Deep dive into Web3 
@@ -26,11 +57,31 @@ const Events = () => {
                        <a href="https://ahack.link/webinar-intro-to-web3"> <BiLink className='text-3xl cursor-pointer' /></a>
                     </div>
                     </div>
-               </div>
+               </div>  */}
 
-        </div>
-        </div>
-    )
-}
+        {events.map((event, index) => (
+          <div className="max-w-xs m-12">
+            <img src={event.img} alt="" className="rounded-3xl" />
+            <h2 className="pt-8 pb-5 font-bold text-xl pl-0">{event.title}</h2>
+            <p className="text-gray-600 pb-3 text-sm">{event.description}</p>
+            <div className="flex items-center justify-between mt-3">
+              <p className="text-gray-400 text-sm">
+                {event.platform} | {event.date} | {event.title}
+              </p>
+              <div className="text-gray-500 flex">
+                <a href={event.eventLink}>
+                  {" "}
+                  <BiLink className="text-3xl cursor-pointer" />
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+
+        
+      </div>
+    </div>
+  );
+};
 
 export default Events;
